47beb4360b3de5c4e4c7f430e86f79b5353c1aab
dharnishp <128043459+dharnishp@users.noreply.github.com>
2025-02-14 22:42:12 +0530

    Update TProgram1.java

--- TProgram1.java
+++ TProgram1.java
@@ -1,1 +1,25 @@
+/*
+Write A Java Program Add two numbers without using ‘+’ operator
+*/
+
+package com.dharnish.tricky;
+import java.util.Scanner;
+class TProgram1
+{
+    public static int add(int a, int b) {
+        while (b != 0) {
+            int carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+      Scanner scn=new Scanner(System.in);  
+      int num1 = scn.nextInt();
+        int num2 = scn.nextInt();
+        System.out.println("Sum: " + add(num1, num2));
+    }
+}
 
